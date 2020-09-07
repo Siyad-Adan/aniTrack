8ea3eb763349cffa2354f08ed8e7c2e4b8a9997c
Siyad Adan <siyadadan@Siyads-MacBook-Pro.local>
2020-09-06 23:13:33 -0400

    added process enviroment variables for mongodb instance credentials

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,8 +1,10 @@
 import { MongoClient } from "mongodb";
-import { dbData } from "./config";
+import { Database } from "../lib";
 
-export const connectDatabase = async (): Promise<{ animes: any }> => {
-  const client = await MongoClient.connect(dbData.MONGODB, {
+const url = `mongodb://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}-shard-00-00.dpeho.mongodb.net:27017,${process.env.DB_CLUSTER}-shard-00-01.dpeho.mongodb.net:27017,${process.env.DB_CLUSTER}-shard-00-02.dpeho.mongodb.net:27017/main?ssl=true&replicaSet=atlas-10mhnk-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+export const connectDatabase = async (): Promise<Database> => {
+  const client = await MongoClient.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
@@ -12,3 +14,5 @@ export const connectDatabase = async (): Promise<{ animes: any }> => {
     animes: db.collection("animes"),
   };
 };
+
+process.env;
