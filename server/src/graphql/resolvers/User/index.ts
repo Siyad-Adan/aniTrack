cf09e12fde580a9f919388846a1424d7da7dba1c
Siyad Adan <siyadadan@Siyads-MacBook-Pro.local>
2020-09-07 17:34:52 -0400

    reorganized resolver to reflect user changes related to animes

---
+++ server/src/graphql/resolvers/User/index.ts
@@ -0,0 +1,91 @@
+import { Anime, User, Database } from "../../../lib";
+import { IResolvers } from "apollo-server-express";
+import { ObjectId } from "mongodb";
+
+export const userResolvers: IResolvers = {
+  Query: {
+    userAnimes: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ): Promise<User[]> => {
+      return await db.users
+        .find({
+          _id: new ObjectId(id),
+        })
+        .toArray();
+    },
+  },
+  Mutation: {
+    deleteAnime: async (
+      _root: undefined,
+      { id, mal_id }: { id: string; mal_id: number },
+      { db }: { db: Database }
+    ): Promise<number> => {
+      const deleteResult = await db.users.updateOne(
+        { _id: new ObjectId(id) },
+        { $pull: { animes: { mal_id } } }
+      );
+
+      if (!deleteResult.result.nModified)
+        throw new Error(`Deletion of anime with id ${mal_id} failed`);
+      return deleteResult.result.nModified;
+    },
+    addAnime: async (
+      _root: undefined,
+      {
+        id,
+        mal_id,
+        image,
+        title,
+        airing,
+        synopsis,
+        type,
+        episodes,
+        rated,
+      }: {
+        id: number;
+        mal_id: number;
+        image: string;
+        title: string;
+        airing: boolean;
+        synopsis: string;
+        type: string;
+        episodes: number;
+        rated: string;
+      },
+      { db }: { db: Database }
+    ): Promise<number> => {
+      console.log("test");
+      try {
+        console.log("test");
+        const addResult = await db.users.updateOne(
+          { _id: new ObjectId(id) },
+          {
+            $push: {
+              animes: {
+                mal_id,
+                image,
+                title,
+                airing,
+                synopsis,
+                type,
+                episodes,
+                rated,
+              },
+            },
+          }
+        );
+        console.log(addResult.result.nModified);
+
+        return addResult.result.nModified;
+      } catch (err) {
+        throw new Error(`Failed to add anime : error ${err}`);
+      }
+    },
+  },
+  User: {
+    //using root here to access parent resolver
+    id: (user: User): string => user._id.toString(),
+  },
+};
